65554144e8cfffbb22c32e584e9e6da52f65a2d2
Rajendra7 <krbabu697@gmail.com>
2020-03-25 21:07:13 +0530

    second commit with updated template

--- src/app/news/news.component.ts
+++ src/app/news/news.component.ts
@@ -7,7 +7,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NewsComponent implements OnInit {
 
-  constructor() { }
+  news : Object[];
+
+  constructor() { 
+    this.news =[
+      {
+        "image":"../../assets/images/story.png",
+        "head" : "Coronavirus LIVE: Even 10,000 ventilators won't help if infections explode, says Delhi health minister",
+        "news" : "The government on Sunday decided to clamp down on the free movement in 80 districts across the country where authorities have reported confirmed cases of the novel coronavirus. On Monday, the death toll due to Covid-19 in India rose to 8. At present, Delhi, Mumbai and Kolkata are observing a lockdown with suspension of metro and other public transport facilities apart."
+      },
+      {
+        "image":"../../assets/images/story.png",
+        "head" : "Coronavirus LIVE: Even 10,000 ventilators won't help if infections explode, says Delhi health minister",
+        "news" : "The government on Sunday decided to clamp down on the free movement in 80 districts across the country where authorities have reported confirmed cases of the novel coronavirus. On Monday, the death toll due to Covid-19 in India rose to 8. At present, Delhi, Mumbai and Kolkata are observing a lockdown with suspension of metro and other public transport facilities apart."
+      }
+    ]
+  }
 
   ngOnInit(): void {
   }
