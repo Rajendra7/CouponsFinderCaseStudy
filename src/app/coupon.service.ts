e9a71dbf31f8ab9b7a6221fdb977934541071c8a
Rajendra7 <krbabu697@gmail.com>
2020-03-30 09:18:34 +0530

    added and able to fetch data

--- src/app/coupon.service.ts
+++ src/app/coupon.service.ts
@@ -8,12 +8,36 @@ import { Coupon } from 'src/modals/coupon';
 export class CouponService {
 
   constructor(private http : HttpClient) { }
-  couponUrl = "../../assets/data/buscoupons.json"
+  buscouponUrl = "../../assets/data/buscoupons.json"
+  flightCouponUrl = "../../assets/data/flightcoupons.json"
+  trainCouponUrl = "../../assets/data/traincoupons.json"
+  hotelCouponUrl ="../../assets/data/hotelcoupons.json"
+  fashionCouponUrl= "../../assets/data/fashioncoupons.json"
+  paymentCouponUrl="../../assets/data/paymentcoupons.json"
+
 
   getBusCoupons() {
-    return this.http.get(this.couponUrl)
+    return this.http.get(this.buscouponUrl)
   }
 
+  getFlightCoupons(){
+   return this.http.get(this.flightCouponUrl) 
+  }
 
+  getTrainCoupons(){
+    return this.http.get(this.trainCouponUrl)
+  }
+
+  getHotelCoupons(){
+    return this.http.get(this.hotelCouponUrl)
+  }
+
+  getFashionCoupons(){
+    return this.http.get(this.fashionCouponUrl)
+  }
+
+  getPaymentCoupons(){
+    return this.http.get(this.paymentCouponUrl)
+  }
 
 }
--- src/app/coupons/coupons.component.ts
+++ src/app/coupons/coupons.component.ts
@@ -10,12 +10,30 @@ import { Coupon } from 'src/modals/coupon';
 export class CouponsComponent implements OnInit {
 
   details:any;
+  flights :any;
+  trains:any;
+  hotels:any;
+  fashions:any;
+  payments:any;
 
   constructor(private couponService : CouponService) { }
 
   ngOnInit(): void {
     this.couponService.getBusCoupons().subscribe(data=>this.details=data);
-    console.log(this.details);
+    
+    this.couponService.getFlightCoupons().subscribe(flight=>this.flights=flight);
+
+    this.couponService.getTrainCoupons().subscribe(train=>this.trains=train);
+
+    this.couponService.getHotelCoupons().subscribe(hotel=>this.hotels=hotel);
+
+    this.couponService.getFashionCoupons().subscribe(fashion=>this.fashions=fashion);
+
+    this.couponService.getPaymentCoupons().subscribe(payment=>this.payments=payment);
+  }
+
+  flip() {
+    
   }
 
   
